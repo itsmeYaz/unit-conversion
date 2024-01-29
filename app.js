b165fc7fc40ae1956f426766bbb1d84541ec1c62
Nielven Mascariñas <mascarinas022@gmail.com>
2024-01-29 20:10:17 +0800

    Refactor unit conversion functions and update result elements

--- app.js
+++ app.js
@@ -2,44 +2,56 @@ const inputEl = document.querySelector("#input-field");
 const convertBtn = document.querySelector("#convert-button");
 const resultEl = document.querySelector(".result-output");
 
-const meterFeet = document.querySelector("#meter-to-feet");
-const literGallon = document.querySelector("#liter-to-gallons");
+const lengthResult = document.querySelector("#length");
+const volumeResult = document.querySelector("#volume");
+const massResult = document.querySelector("#mass");
 
 convertBtn.addEventListener("click", () => {
   let inputValue = Number(inputEl.value);
+  inputEl.value = "";
   //convert meters to feet
-  meterFeet.textContent = `${inputValue} meters = ${meterToFeet(
+  lengthResult.textContent = `${inputValue} meters = ${meterToFeet(
     inputValue
   )} | ${inputValue} feet = ${feetToMeter(inputValue)} meters`;
 
-  literGallon.textContent = `${inputValue} gallons = ${literToGallon(
+  volumeResult.textContent = `${inputValue} gallons = ${literToGallon(
     inputValue
   )} | ${inputValue} gallons = ${galToLiter(inputValue)} liters`;
 
-  inputEl.value = "";
+  massResult.textContent = `${inputValue} kilos = ${kilogramToPound(
+    inputValue
+  )} | ${inputValue} pounds = ${poundToKg(inputValue)} kilos`;
 });
 
+//Length functions
 function meterToFeet(m) {
-  let feet = m * 3.281;
-  return feet;
+  let feet = (m * 3.28084).toFixed(2);
+  return Number(feet);
 }
 
 function feetToMeter(f) {
-  let meter = f * 0.3048;
-  return meter;
+  let meter = (f * 0.3048).toFixed(2);
+  return Number(meter);
 }
 
+//Volume Functions
 function literToGallon(l) {
-  let galoon = l * 0.264;
-  return galoon;
+  let galoon = (l * 0.264172).toFixed(2);
+  return Number(galoon);
 }
 
 function galToLiter(gal) {
-  let liter = gal * 3.785;
-  return liter;
+  let liter = (gal * 3.78541).toFixed(2);
+  return Number(liter);
 }
 
+//Mass Functions
 function kilogramToPound(kg) {
-  let pound = kg * 2.204;
-  return pound;
+  let pound = (kg * 2.20462).toFixed(2);
+  return Number(pound);
+}
+
+function poundToKg(pound) {
+  let kg = (pound * 0.453592).toFixed(2);
+  return Number(kg);
 }
